7c4996c47961619a42be37f05a225afe0c7161b5
Megham Garg <gargmegham3@gmail.com>
2023-04-29 19:18:43 +0530

    add safe guards

--- bot/handlers/registeration.py
+++ bot/handlers/registeration.py
@@ -34,11 +34,12 @@ async def start(update: Update, context: CallbackContext) -> int:
 async def gender(update: Update, context: CallbackContext) -> int:
     """Stores the selected gender and asks for age."""
     user = update.message.from_user
-    mysql_db.set_user_attribute(
-        user.id,
-        "gender",
-        update.message.text,
-    )
+    if update.message.text in ["Male", "Female", "Other"]:
+        mysql_db.set_user_attribute(
+            user.id,
+            "gender",
+            update.message.text,
+        )
     await update.message.reply_text("I see! Please tell me your age.")
     return AGE
 
@@ -46,11 +47,12 @@ async def gender(update: Update, context: CallbackContext) -> int:
 async def age(update: Update, context: CallbackContext) -> int:
     """Stores the selected age and asks for allergies."""
     user = update.message.from_user
-    mysql_db.set_user_attribute(
-        user.id,
-        "age",
-        update.message.text,
-    )
+    if update.message.text.isdigit():
+        mysql_db.set_user_attribute(
+            user.id,
+            "age",
+            update.message.text,
+        )
     await update.message.reply_text(
         "Please tell me about your allergies if any, or send /skip if you don't have any.\n\You can send multiple allergies just copy below format, and modify it according to your details.\n\n<code>Allergies: pollen, juniper, dust mites, mold, cats, dogs, cockroaches</code>",
         parse_mode=ParseMode.HTML,
@@ -70,10 +72,11 @@ async def allergies(update: Update, context: CallbackContext) -> int:
     except Exception:
         allergies = []
     for allergy in allergies:
-        mysql_db.add_new_allergy(
-            user.id,
-            allergy.strip(),
-        )
+        if allergy.strip():
+            mysql_db.add_new_allergy(
+                user.id,
+                allergy.strip(),
+            )
     await update.message.reply_text(
         "Please tell me about your medical history, or send /skip if you don't have any.\nYou can send multiple medical conditions copy below format, and modify it according to your details.\n\n<code>Condition: diabetes\nFrom: 2010-01-01\nTo: 2015-01-01\nSurgeries Performed: gastric bypass, gallbladder removal\nSymptoms: fatigue, weight loss, frequent urination\nMedications: metformin, insulin, lantus\n</code>",
         parse_mode=ParseMode.HTML,
@@ -130,15 +133,16 @@ async def medical_history(update: Update, context: CallbackContext) -> int:
                 medications = line.split(":")[1].strip()
             except IndexError:
                 medications = ""
-    mysql_db.add_new_medical_history(
-        user.id,
-        condition,
-        from_date,
-        to_date,
-        surgeries_performed,
-        symptoms,
-        medications,
-    )
+    if condition:
+        mysql_db.add_new_medical_history(
+            user.id,
+            condition,
+            from_date,
+            to_date,
+            surgeries_performed,
+            symptoms,
+            medications,
+        )
     await update.message.reply_text(
         "I'll take note of that. Now, send me your location please, or send /skip."
     )
@@ -148,7 +152,7 @@ async def medical_history(update: Update, context: CallbackContext) -> int:
 async def skip_medical_history(update: Update, context: CallbackContext) -> int:
     """Skips the medical history and asks for location."""
     await update.message.reply_text(
-        "You seem a bit paranoid! Now, send me your location please, or send /skip."
+        "Be at ease your data is not going anywhere! Now, send me your location please, or send /skip."
     )
     return LOCATION
 
@@ -157,9 +161,10 @@ async def location(update: Update, context: CallbackContext) -> int:
     """Stores the location and ends the conversation."""
     user = update.message.from_user
     user_location = update.message.location
-    mysql_db.set_user_attribute(
-        user.id, "address", f"{user_location.latitude}, {user_location.longitude}"
-    )
+    if user_location:
+        mysql_db.set_user_attribute(
+            user.id, "address", f"{user_location.latitude}, {user_location.longitude}"
+        )
     await update.message.reply_text(
         "Thank you! I will take all of this into account while assisting you in the future."
     )
