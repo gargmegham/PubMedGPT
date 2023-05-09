d41ebed281e89e57f13667cdc166de238590b550
Megham Garg <gargmegham3@gmail.com>
2023-05-09 13:02:19 +0530

    fixed lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 import sqlalchemy
 
 
@@ -8,43 +9,47 @@ def lambda_handler(event, context):
     triggered by cal.com webhook
     """
     try:
-        MYSQL_USER=os.environ['MYSQL_USER']
-        MYSQL_PASSWORD=os.environ['MYSQL_PASSWORD']
-        MYSQL_PORT=os.environ['MYSQL_PORT']
-        MYSQL_DATABASE=os.environ['MYSQL_DATABASE']
-        MYSQL_HOST=os.environ['MYSQL_HOST']
-        payload = json.loads(event['body'])
+        MYSQL_USER = os.environ["MYSQL_USER"]
+        MYSQL_PASSWORD = os.environ["MYSQL_PASSWORD"]
+        MYSQL_PORT = os.environ["MYSQL_PORT"]
+        MYSQL_DATABASE = os.environ["MYSQL_DATABASE"]
+        MYSQL_HOST = os.environ["MYSQL_HOST"]
+        event_body = event["body"]
+        payload = json.loads(event_body)
         engine = sqlalchemy.create_engine(
             sqlalchemy.engine.url.URL(
                 drivername="mysql+pymysql",
                 username=MYSQL_USER,
                 password=MYSQL_PASSWORD,
                 host=MYSQL_HOST,
-                port=MYSQL_PORT,
+                port=int(MYSQL_PORT),
                 database=MYSQL_DATABASE,
+                query={"charset": "utf8mb4"},
             ),
         )
-        username = payload['payload']['responses']['name']['value']
-        event_id = payload['payload']['uid']
+        username = str(payload["payload"]["responses"]["name"]["value"])
+        event_id = str(payload["payload"]["uid"])
         with engine.connect() as conn:
-            if payload['triggerEvent'] == 'BOOKING_CREATED':
+            if payload["triggerEvent"] == "BOOKING_CREATED":
                 user_id = conn.execute(
-                    f"SELECT user_id FROM user WHERE username = '{username}'"
+                    sqlalchemy.text(
+                        f"SELECT user_id FROM user WHERE username = '{username}'"
+                    )
                 ).fetchone()[0]
-                conn.execute(
-                    f"INSERT INTO booking (user_id, event_id) VALUES ({user_id}, '{event_id}')"
+                res = conn.execute(
+                    sqlalchemy.text(
+                        f"INSERT INTO booking (user_id, event_id, timestamp) VALUES ({user_id}, '{event_id}', NOW())"
+                    )
                 )
-            elif payload['triggerEvent'] == 'BOOKING_CANCELLED':
-                conn.execute(
-                    f"DELETE FROM booking WHERE event_id = '{event_id}'"
+                conn.commit()
+            elif payload["triggerEvent"] == "BOOKING_CANCELLED":
+                res = conn.execute(
+                    sqlalchemy.text(
+                        f"DELETE FROM booking WHERE event_id = '{event_id}'"
+                    )
                 )
-            conn.commit()
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Completed successfully')
-        }
-    except:
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Failed to complete')
-        }
+                conn.commit()
+        return {"statusCode": 200, "body": json.dumps("Completed successfully")}
+    except Exception as err:
+        print(err)
+        return {"statusCode": 500, "body": json.dumps("Failed to complete")}
