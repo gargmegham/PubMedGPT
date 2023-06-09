8247b645a05d4dd7bf66d6dff0c146519ea1a908
Megham Garg <gargmegham3@gmail.com>
2023-06-09 10:52:51 +0530

    bug fix

--- bot/mysql.py
+++ bot/mysql.py
@@ -318,8 +318,10 @@ class MySQL:
 
     def write_prescription(self, user_id: int, disease_id: int = None) -> list:
         def any_word_in_x_match_any_word_in_y(x: list, y: str):
-            list1 = [str(_).lower().strip() for _ in str(x).split(",")]
+            list1 = []
             list2 = [str(_).lower().strip() for _ in str(y).split(",")]
+            for sentence in x:
+                list1.extend([str(_).lower().strip() for _ in str(sentence).split(",")])
             for word in list1:
                 if word in list2:
                     return True
